560a3f97c0fc82c3db43545bd6ceb2239ce2971e
asanti <asantidrian83@gmail.com>
2022-08-21 21:44:29 +1000

    addUser mutation added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
+  mutation loginUser($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation createUser($email: String!, $password: String!, $username: String!) {
+  mutation addUser($email: String!, $password: String!, $username: String!) {
     createUser(email: $email, password: $password, username: $username) {
       token
       user {
